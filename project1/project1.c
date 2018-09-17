81a18f7306e4bff265d4b7db7483028701da8600
Daniel Patrick Gibney <gibney@linprog2.cs.fsu.edu>
2018-09-17 16:58:31 -0400

    quick execv implementation

--- project1/project1.c
+++ project1/project1.c
@@ -1,69 +1,97 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 char** addToken(char** instr, char* tok, int numTokens);
 void printTokens(char** instr, int numTokens);
  
 int main() {
-    char token[256];      	// holds instruction token
-    char ** bucket;			// array that holds all instruction tokens
-    char temp[256];			// used to split instruction tokens containing special characters
-	
-    while (1) {
-		printf("Please enter an instruction:");
+        char token[256];      	// holds instruction token
+        char ** bucket;			// array that holds all instruction tokens
+        char temp[256];			// used to split instruction tokens containing special characters
+        int pid;
 
-        int numI = 0;                // number of tokens in an instruction
-		
-        do {                            // loop reads character sequences separated by whitespace
-            scanf( "%s", token);
-			
-			int i;
-			int start;
-			
-			start = 0;
-			
-			for (i = 0; i < strlen(token); i++)
-			{
-				if (token[i] == '|' || token[i] == '>' || token[i] == '<' || token[i] == '&') 
-				{
-					if (i-start > 0)
-					{
-						memcpy(temp, token + start, i - start);
-						temp[i-start] = '\0';
-						bucket = addToken(bucket, temp, numI);
-						numI++;
-						
-					}
-					
-					char specialChar[2];
-					specialChar[0] = token[i];
-					specialChar[1] = '\0';
-					
-					bucket = addToken(bucket,specialChar,numI);
-					numI++;
-					
-					start = i + 1;
-				}
-			}
-			if (start < strlen(token))
-			{
-				memcpy(temp, token + start, strlen(token) - start);
-				temp[i-start] = '\0';
-				bucket = addToken(bucket, temp, numI);
-				numI++;
-				
-			}
-		
-        } while ('\n' != getchar());    //until end of line is reached
-		
-		printTokens(bucket, numI);
-		
-    }  //until "exit" is read in
-	free(bucket);	//free dynamic memory
-    printf("Exiting...\n");
+        while (1) {
+                printf("Please enter an instruction:");
 
-    return 0;
+                int numI = 0;                // number of tokens in an instruction
+
+                do {                            // loop reads character sequences separated by whitespace
+                        scanf( "%s", token);
+
+                        int i;
+                        int start;
+
+                        start = 0;
+
+                        for (i = 0; i < strlen(token); i++)
+                        {
+                                if (token[i] == '|' || token[i] == '>' || token[i] == '<' || token[i] == '&') 
+                                {
+                                        if (i-start > 0)
+                                        {
+                                                memcpy(temp, token + start, i - start);
+                                                temp[i-start] = '\0';
+                                                bucket = addToken(bucket, temp, numI);
+                                                numI++;
+
+                                        }
+
+                                        char specialChar[2];
+                                        specialChar[0] = token[i];
+                                        specialChar[1] = '\0';
+
+                                        bucket = addToken(bucket,specialChar,numI);
+                                        numI++;
+
+                                        start = i + 1;
+                                }
+                        }
+                        if (start < strlen(token))
+                        {
+                                memcpy(temp, token + start, strlen(token) - start);
+                                temp[i-start] = '\0';
+                                bucket = addToken(bucket, temp, numI);
+                                numI++;
+
+                        }
+
+                } while ('\n' != getchar());    //until end of line is reached
+
+                printTokens(bucket, numI);
+                if ((pid = fork()) == 0)
+                {
+                        // this is the forked child process that is a copy of the running program
+                       // if(newin == false){
+                       // dup2(tty, 0); // force stdin from tty
+                       // }else{
+                       // dup2(fdin, 0);
+                       // close(fdin);
+                       // }
+                       // if(newout == false){
+                       // dup2(tty, 1); // force stdout to tty
+                       // }else{
+                       // dup2(fdout, 1);
+                       // close(fdout);
+                       // }
+                       // dup2(fdout, 2); // force stderr to tty
+                       // close(tty);
+                       // // last argument must be NULL for execvp()
+                       // av[ac] = NULL;
+                        // execute program av[0] with arguments av[0]... replacing this program
+                        execv(bucket[0],(char *[]){bucket[0],NULL });
+                        if(pid==0){
+                            printf("whoops\n");
+                        }
+                        //fprintf(stderr, "can't execute %s\n", av[0]);
+                        exit(EXIT_FAILURE);
+                        } 
+
+        }  //until "exit" is read in
+        free(bucket);	//free dynamic memory
+        printf("Exiting...\n");
+
+        return 0;
 }
 
 //reallocates instruction array to hold another token,
