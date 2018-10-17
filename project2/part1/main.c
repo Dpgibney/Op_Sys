9d83909ba6090f599e77d5acdda093a25de6c1d1
Daniel Gibney <dpgibney@gmail.com>
2018-10-17 16:53:41 -0400

    I think part 1 is done?

--- project2/part1/main.c
+++ project2/part1/main.c
@@ -1,4 +1,15 @@
 #include<unistd.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 int main(){
+        FILE *fptr;
+	char* str = (char*)malloc(100*sizeof(char));
+	fptr = fopen("example.txt","w+r+");
+	fprintf(fptr,"test line 1 \n");
+	fprintf(fptr,"test line 2 \n");
+	fprintf(fptr,"test line 3 \n");
+	rewind(fptr);
+	fscanf(fptr,"%s",str);
+	printf("%s\n",str);
+	fclose(fptr);
 }
