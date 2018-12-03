a82e9db9a238b838c771b34d5334bfd415789a87
Daniel Patrick Gibney <dpgibney@gmail.com>
2018-12-03 17:00:58 -0500

    struct for info

--- project3/main.c
+++ project3/main.c
@@ -2,6 +2,35 @@
 #include<string.h>
 #include<stdlib.h>
 #define MAX_INPUT_SIZE 200
+
+struct BPB_2_0{
+    unsigned short bytes_per_logic;
+    char logial_sectors_per_cluster;
+    unsigned short reserved_logical_sectors;
+    char num_file_allocation_tables;
+    unsigned short Max_num_fat12_16_root_entries;
+    unsigned short total_logical_sectors;
+    char media_descriptor; 
+    unsigned short logic_sectors_fat12_16;
+};
+
+struct BPB_3_31{
+   struct BPB_2_0 BPB_2;
+   unsigned short Physical_sectors_per_track;
+   unsigned short number_of_heads;
+   unsigned int count_hidden_sectors;
+   unsigned int total_logical_sectors;
+};
+struct boot_sector_struct{
+   struct BPB_3_31 BPB_3;
+   char drive_number;
+   char reserved;
+   char extended_boot_signature;
+   unsigned int volume_id;
+   char* volume_partition_label;
+   char* file_system_type;
+};
+
 int main(int argc,char *argv[]){
 char* input_raw = (char*)malloc(MAX_INPUT_SIZE*sizeof(char));
 char* input = (char*)malloc(MAX_INPUT_SIZE*sizeof(char));
@@ -14,6 +43,13 @@ if(argc!=2){
    printf("Improper usage: must pass image file as argument\n");
    return -1;
 }
+
+//creating struct to hold info about filesystem one extra space for endline;
+struct boot_sector_struct info;
+info.volume_partition_label = (char*)malloc(12);
+info.file_system_type = (char*)malloc(9);
+
+
 //check that file could open
 //currently using binary mode
 fptr = fopen(argv[1],"rb+");
@@ -38,5 +74,10 @@ sscanf(input_raw,"%s",input);
 //free memory here
 free(input);
 free(input_raw);
+
+//TODO free malloc'd memory in struct
+
+//close file
+close(fptr);
 
 }
